7ae8334963e335a7e1115bd9b56310e277d81c56
RacketyAlmond <papomykalski@gmail.com>
2025-04-11 23:52:00 +0200

    Modified api endpoint

--- app/api/cabins/[cabinId]/route.js
+++ app/api/cabins/[cabinId]/route.js
@@ -1,3 +1,17 @@
+import { getBookedDatesByCabinId } from "@/app/_lib/data-service";
+
 export async function GET(request, {params}) {
-    return Response.json({test:"test"});
+    const {cabinId} = params;
+    try {
+        const [cabin, bookedDates] = await
+        Promise.all([
+            getCabin(cabinId),
+            getBookedDatesByCabinId(cabinId),
+        ]);
+        return Response.json({cabin, bookedDates});
+
+    }
+    catch {
+        return Response.json({message: "Cabin not found"});
+    }
 }
