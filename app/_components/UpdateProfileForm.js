92e8423c675d07a7665be0a454fc58581ff78364
RacketyAlmond <papomykalski@gmail.com>
2025-06-09 06:23:51 +0200

    Deleting reservations

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -2,13 +2,15 @@
 
 import React, { useState } from 'react'
 import { updateGuest } from '../_lib/actions';
+import { useFormStatus } from 'react-dom';
 
 
 
 export default function UpdateProfileForm({guest, children}) {
-    const [count, setCount] = useState();
-    const {fullName, email, nationality, nationalID, countryFlag} = guest;
+  const [count, setCount] = useState();
+  const {fullName, email, nationality, nationalID, countryFlag} = guest;
   
+
   return (
     <div>
         <form action={updateGuest} className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
@@ -56,12 +58,19 @@ export default function UpdateProfileForm({guest, children}) {
         </div>
 
         <div className="flex justify-end items-center gap-6">
-          <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-            Update profile
-          </button>
+          <Button />
         </div>
       </form>
 
     </div>
   )
+}
+
+function Button() {
+  const {pending} = useFormStatus();
+  return (
+    <button disabled={pending} className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
+      {pending ? `Updating ...` : `Update profile`}
+    </button>
+  );
 }
